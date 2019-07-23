9659a059f068c248aacbcf557e5185169a06bd91
operasfantom <operasfantom@gmail.com>
2019-07-23 16:39:11 +0300

    small fixes due to version upgrade

--- protocol/src/main/kotlin/model/editorPlugin/RdEditorModel.kt
+++ protocol/src/main/kotlin/model/editorPlugin/RdEditorModel.kt
@@ -4,14 +4,14 @@ import com.jetbrains.rd.generator.nova.*
 import com.jetbrains.rd.generator.nova.PredefinedType.*
 import com.jetbrains.rd.generator.nova.cpp.Cpp17Generator
 import com.jetbrains.rd.generator.nova.csharp.CSharp50Generator
-import com.jetbrains.rd.generator.nova.util.syspropertyOrEmpty
+import com.jetbrains.rd.generator.nova.util.syspropertyOrInvalid
 import java.io.File
 
 @Suppress("unused")
-object RdEditorModel: Root(
-        CSharp50Generator(FlowTransform.AsIs, "JetBrains.Platform.UnrealEngine.Model", File(syspropertyOrEmpty("model.out.src.editorPlugin.csharp.dir"))),
-        Cpp17Generator(FlowTransform.Reversed, "com_jetbrains_rider_plugins_unrealengine", File(syspropertyOrEmpty("model.out.src.editorPlugin.cpp.dir")))
-){
+object RdEditorModel : Root(
+        CSharp50Generator(FlowTransform.AsIs, "JetBrains.Platform.UnrealEngine.Model", File(syspropertyOrInvalid("model.out.src.editorPlugin.csharp.dir"))),
+        Cpp17Generator(FlowTransform.Reversed, "com.jetbrains.rider.plugins.unrealengine", File(syspropertyOrInvalid("model.out.src.editorPlugin.cpp.dir")))
+) {
     init {
 
         property("test_connection", int.nullable)
