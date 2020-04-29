84788388e6bb86dd0a90d500e9574a2771cafca3
Alexander Pirogov <a.g.pirogov@gmail.com>
2020-04-29 15:06:21 +0300

    Merging changes from net201

--- src/cpp/RiderLink/Source/RiderLoggingExtension/Private/RiderLoggingExtension.cpp
+++ src/cpp/RiderLink/Source/RiderLoggingExtension/Private/RiderLoggingExtension.cpp
@@ -1,6 +1,7 @@
 #include "RiderLoggingExtension.hpp"
 
 #include "RiderLink.hpp"
+#include "RdEditorProtocol/UE4Library/LogMessageInfo.h"
 #include "RdEditorProtocol/UE4Library/UnrealLogEvent.h"
 
 #include "Misc/DateTime.h"
@@ -14,24 +15,25 @@ IMPLEMENT_MODULE(FRiderLoggingExtensionModule, RiderLoggingExtension);
 
 void FRiderLoggingExtensionModule::StartupModule()
 {
-    UE_LOG(FLogRiderLoggingExtensionModule, Log, TEXT("STARTUP START"));
+    UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("STARTUP START"));
 
-    static const auto START_TIME = FDateTime::Now();
+    static const auto START_TIME = FDateTime::UtcNow();
     static const auto GetTimeNow = [](double Time) -> rd::DateTime
     {
         return rd::DateTime(static_cast<std::time_t>(START_TIME.ToUnixTimestamp() +
             static_cast<int64>(Time)));
     };
+
     outputDevice.onSerializeMessage.BindLambda(
-        [this](const TCHAR* Message, ELogVerbosity::Type Type,
+        [this](const TCHAR* msg, ELogVerbosity::Type Type,
                const class FName& Name, TOptional<double> Time)
         {
             if (Type > ELogVerbosity::All) return;
 
             FRiderLinkModule& RiderLinkModule = FModuleManager::GetModuleChecked<FRiderLinkModule>(
                 FRiderLinkModule::GetModuleName());
-            RiderLinkModule.rdConnection.Scheduler.queue(
-                [this, message = FString(Message), Type,
+
+            RiderLinkModule.RdConnection.Scheduler.queue([this, tail = FString(msg), Type,
                     Name = Name.GetPlainNameString(),
                     Time]() mutable
                 {
@@ -40,24 +42,37 @@ void FRiderLoggingExtensionModule::StartupModule()
                     {
                         DateTime = GetTimeNow(Time.GetValue());
                     }
-                    auto MessageInfo = Jetbrains::EditorPlugin::LogMessageInfo(
-                        Type, Name, DateTime);
+                    Jetbrains::EditorPlugin::LogMessageInfo MessageInfo =
+                        Jetbrains::EditorPlugin::LogMessageInfo(Type, Name, DateTime);
+
                     FRiderLinkModule& RiderLinkModule = FModuleManager::GetModuleChecked<
                         FRiderLinkModule>(
                         FRiderLinkModule::GetModuleName());
-                    RiderLinkModule.rdConnection.UnrealToBackendModel.get_unrealLog().fire(
+
+                    FString toSend;
+                    while (tail.Split("\n", &toSend, &tail))
+                    {
+                        toSend.TrimEndInline();
+                        RiderLinkModule
+                            .RdConnection.UnrealToBackendModel.get_unrealLog().fire(
+                                Jetbrains::EditorPlugin::UnrealLogEvent{
+                                    std::move(MessageInfo), std::move(toSend)
+                                });
+                    }
+                    tail.TrimEndInline();
+                    RiderLinkModule.RdConnection.UnrealToBackendModel.get_unrealLog().fire(
                         Jetbrains::EditorPlugin::UnrealLogEvent{
-                            std::move(MessageInfo), std::move(message)
+                            MessageInfo, std::move(tail)
                         });
                 });
         });
 
-    UE_LOG(FLogRiderLoggingExtensionModule, Log, TEXT("STARTUP FINISH"));
+    UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("STARTUP FINISH"));
 }
 
 void FRiderLoggingExtensionModule::ShutdownModule()
 {
-    UE_LOG(FLogRiderLoggingExtensionModule, Log, TEXT("SHUTDOWN START"));
+    UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("SHUTDOWN START"));
     outputDevice.onSerializeMessage.Unbind();
-    UE_LOG(FLogRiderLoggingExtensionModule, Log, TEXT("SHUTDOWN FINISH"));
+    UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("SHUTDOWN FINISH"));
 }
