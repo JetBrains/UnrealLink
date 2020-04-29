d7c2220804a07e49ca7bf35cf901d846ab216418
Alexander Pirogov <a.g.pirogov@gmail.com>
2020-04-29 15:39:46 +0300

    Adding wrappers for includes with windows API

--- src/cpp/RiderLink/Source/RiderLink/Public/RdConnection.hpp
+++ src/cpp/RiderLink/Source/RiderLink/Public/RdConnection.hpp
@@ -2,9 +2,19 @@
 
 #pragma once
 
+#if PLATFORM_WINDOWS
+// ReSharper disable once CppUnusedIncludeDirective
+#include "Windows/AllowWindowsPlatformTypes.h"
+#include "Windows/PreWindowsApi.h"
+
 #include "rd_framework_cpp/base/IProtocol.h"
 #include "rd_framework_cpp/scheduler/SingleThreadScheduler.h"
 #include "RdEditorProtocol/RdEditorModel/RdEditorModel.h"
+
+#include "Windows/PostWindowsApi.h"
+// ReSharper disable once CppUnusedIncludeDirective
+#include "Windows/HideWindowsPlatformTypes.h"
+#endif
 
 #include "Templates/UniquePtr.h"
 
