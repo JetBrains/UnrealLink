cf3d835dc1678ea3d6cee34f519a41a96397783e
Ivan Donchevskii <ivan.donchevskii@jetbrains.com>
2020-09-14 17:35:46 +0200

    RIDER-50354: Don't create a separate console for time
    The solution was not perfect and lead to many mismatches
    in scrolling. Let's switch time on/off the same way we filter.

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/UnrealPane.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/UnrealPane.kt
@@ -7,7 +7,6 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.ui.components.JBCheckBox
@@ -19,15 +18,12 @@ import com.jetbrains.rider.plugins.unreal.actions.FilterComboAction
 import com.jetbrains.rider.ui.components.ComponentFactories
 import java.awt.BorderLayout
 import javax.swing.JPanel
-import javax.swing.JScrollPane
 
 class UnrealPane(val model: Any, lifetime: Lifetime, val project: Project) : SimpleToolWindowPanel(false) {
     private val consoleView: ConsoleViewImpl = ComponentFactories.getConsoleView(project, lifetime)
-    private val timeConsoleView: ConsoleViewImpl = ComponentFactories.getConsoleView(project, lifetime)
 
     companion object {
         lateinit var currentConsoleView : ConsoleViewImpl
-        lateinit var currentTimeConsoleView : ConsoleViewImpl
         val verbosityFilterActionGroup: FilterComboAction = FilterComboAction("Verbosity")
         val categoryFilterActionGroup: FilterComboAction = FilterComboAction("Categories")
         val timestampCheckBox: JBCheckBox = JBCheckBox("Show timestamps", false)
@@ -38,33 +34,15 @@ class UnrealPane(val model: Any, lifetime: Lifetime, val project: Project) : Sim
     }
 
     init {
-        currentTimeConsoleView = timeConsoleView
-        (timeConsoleView.editor as EditorImpl).scrollPane.verticalScrollBar.isVisible = false
-        (timeConsoleView.editor as EditorImpl).scrollPane.verticalScrollBar.isEnabled = false
-        (timeConsoleView.editor as EditorImpl).scrollPane.verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_NEVER;
-        timeConsoleView.isVisible = false
-
         currentConsoleView = consoleView
-        consoleView.setUpdateFoldingsEnabled(true)
-        consoleView.editor.scrollingModel.addVisibleAreaListener {
-            timeConsoleView.editor.scrollingModel.scrollVertically(it.newRectangle.y)
-        }
 
         currentConsoleView.editor.document.addDocumentListener(object : DocumentListener {
             override fun documentChanged(event: DocumentEvent) {
                 if (event.oldLength > 0 && event.newLength == 0) {
-                    currentTimeConsoleView.editor.document.deleteString(0, currentTimeConsoleView.editor.document.textLength)
                     logSize = 0
                     if (!filteringInProgress) {
                         logData.clear()
                     }
-                }
-            }
-        })
-        currentTimeConsoleView.editor.document.addDocumentListener(object : DocumentListener {
-            override fun documentChanged(event: DocumentEvent) {
-                if (event.oldLength > 0 && event.newLength == 0) {
-                    currentConsoleView.editor.document.deleteString(0, currentConsoleView.editor.document.textLength)
                 }
             }
         })
@@ -97,7 +75,6 @@ class UnrealPane(val model: Any, lifetime: Lifetime, val project: Project) : Sim
         consoleView.scrollTo(0)
 
         consoleView.add(topPanel, BorderLayout.NORTH)
-        consoleView.add(timeConsoleView, BorderLayout.WEST)
         setContent(consoleView)
         setToolbar(toolbar)
     }
