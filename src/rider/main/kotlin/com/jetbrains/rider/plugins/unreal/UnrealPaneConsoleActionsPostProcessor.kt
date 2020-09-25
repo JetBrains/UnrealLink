315e559bae7fb5a322ee3b93539b32bca822ce92
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2020-09-25 10:26:17 +0300

    Use ClearThisConsoleAction implementation for ClearUnrealConsoleAction instead of ClearConsoleAction

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/UnrealPaneConsoleActionsPostProcessor.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/UnrealPaneConsoleActionsPostProcessor.kt
@@ -1,13 +1,16 @@
 package com.jetbrains.rider.plugins.unreal
 
+import com.intellij.execution.ExecutionBundle
 import com.intellij.execution.actions.ClearConsoleAction
 import com.intellij.execution.actions.ConsoleActionsPostProcessor
 import com.intellij.execution.impl.ConsoleViewImpl
 import com.intellij.execution.ui.ConsoleView
+import com.intellij.icons.AllIcons
 import com.intellij.ide.actions.NextOccurenceToolbarAction
 import com.intellij.ide.actions.PreviousOccurenceToolbarAction
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 
 class UnrealPaneConsoleActionsPostProcessor : ConsoleActionsPostProcessor() {
     override fun postProcess(console: ConsoleView, actions: Array<AnAction>): Array<AnAction> {
@@ -55,11 +58,20 @@ class UnrealPaneConsoleActionsPostProcessor : ConsoleActionsPostProcessor() {
         return filteredActions.toTypedArray()
     }
 
-    private class ClearUnrealConsoleAction(val consolePanel: UnrealPane) : ClearConsoleAction() {
+    private class ClearUnrealConsoleAction(private val consolePanel: UnrealPane) :
+            DumbAwareAction(ExecutionBundle.messagePointer("clear.all.from.console.action.name"),
+                    ExecutionBundle.messagePointer("clear.all.from.console.action.description"),
+                    AllIcons.Actions.GC) {
+
+        override fun update(e: AnActionEvent) {
+            val console = UnrealPane.currentConsoleView
+            val enabled: Boolean = console.contentSize > 0
+            e.presentation.isEnabled = enabled
+        }
+
         override fun actionPerformed(e: AnActionEvent) {
             UnrealPane.logData.clear()
-
-            super.actionPerformed(e)
+            UnrealPane.currentConsoleView.clear()
         }
     }
 }
