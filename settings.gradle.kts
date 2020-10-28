b7c337f4c1d40e7bc614d441a6361bdfa56ef7b6
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2020-10-28 20:48:08 +0300

    Use pluginManagement instead of buildscript for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+pluginManagement {
+    repositories {
+        maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
+        maven { setUrl("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-eap") }
+        maven { setUrl("https://cache-redirector.jetbrains.com/myget.org.rd-snapshots.maven") }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.jetbrains.rdgen") {
+                useModule("com.jetbrains.rd:rd-gen:${requested.version}")
+            }
+        }
+    }
+}
+
 rootProject.name = "UnrealLink"
 
 include(":protocol")
