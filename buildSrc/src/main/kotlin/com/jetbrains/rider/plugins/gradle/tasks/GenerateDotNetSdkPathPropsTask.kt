a33d714461c80ecf90423520356770fb89ab6233
Alexander Pirogov <a.g.pirogov@gmail.com>
2020-11-06 16:09:31 +0300

    Stealing helper tasks from resharper-unity

---
+++ buildSrc/src/main/kotlin/com/jetbrains/rider/plugins/gradle/tasks/GenerateDotNetSdkPathPropsTask.kt
@@ -0,0 +1,28 @@
+package com.jetbrains.rider.plugins.gradle.tasks
+
+import com.jetbrains.rider.plugins.gradle.buildServer.buildServer
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class GenerateDotNetSdkPathPropsTask: DefaultTask() {
+    @Input
+    var dotNetSdkPath: Any? = null
+
+    @OutputFile
+    var propsFile = File("${project.projectDir}/../resharper/build/generated/DotNetSdkPath.generated.props")
+
+    @TaskAction
+    fun generate() {
+        val dotNetSdkFile= dotNetSdkPath?.let { project.file(it)} ?: error("dotNetSdkLocation not set")
+        assert(dotNetSdkFile.isDirectory)
+        project.buildServer.progress("Generating :${propsFile.canonicalPath}...")
+        project.file(propsFile).writeText("""<Project>
+          <PropertyGroup>
+            <DotNetSdkPath>${dotNetSdkFile.canonicalPath}</DotNetSdkPath>
+          </PropertyGroup>
+        </Project>""".trimIndent())
+    }
+}
