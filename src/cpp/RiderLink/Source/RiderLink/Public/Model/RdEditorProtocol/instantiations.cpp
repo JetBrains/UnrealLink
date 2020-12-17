fd20b77ca8e633aae5668f2f12fcbf759eaecae1
Alexander Pirogov <a.g.pirogov@gmail.com>
2020-12-17 15:09:03 +0300

    Get rid of multiple template class instantiations

---
+++ src/cpp/RiderLink/Source/RiderLink/Public/Model/RdEditorProtocol/instantiations.cpp
@@ -0,0 +1,4 @@
+#include "instantiations.h"
+
+namespace rd {
+}
---
+++ src/cpp/RiderLink/Source/RiderLink/Public/Model/RdEditorProtocol/instantiations.h
@@ -0,0 +1,13 @@
+#ifndef INSTANTIATIONS_H
+#define INSTANTIATIONS_H
+
+#include "serialization/Polymorphic.h"
+
+
+extern template class rd::Wrapper<std::wstring>;
+
+
+namespace rd {
+}
+
+#endif // INSTANTIATIONS_H
