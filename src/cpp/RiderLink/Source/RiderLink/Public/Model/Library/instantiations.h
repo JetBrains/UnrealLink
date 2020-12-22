4b761cfd58683b4dbf5cf981c3132c50f94dcf16
Alexander Pirogov <a.g.pirogov@gmail.com>
2020-12-22 17:15:19 +0300

    Move instantiations to header

--- src/cpp/RiderLink/Source/RiderLink/Public/Model/Library/instantiations.h
+++ src/cpp/RiderLink/Source/RiderLink/Public/Model/Library/instantiations.h
@@ -12,11 +12,41 @@ enum Type : uint8;
 }
 
 namespace rd {
-template <>
-class RIDERLINK_API Polymorphic<ELogVerbosity::Type> {
+
+template<>
+class RIDERLINK_API Polymorphic<ELogVerbosity::Type, void> {
     public:
-    static ELogVerbosity::Type read(SerializationCtx& ctx, Buffer& buffer);
-    static void write(SerializationCtx& ctx, Buffer& buffer, ELogVerbosity::Type const& value);
+    static ELogVerbosity::Type read(SerializationCtx& ctx, Buffer& buffer) {
+        int32_t x = buffer.read_integral<int32_t>();
+        switch (x) {
+        case 10:
+            return ELogVerbosity::Type::VerbosityMask;
+        case 11:
+            return ELogVerbosity::Type::SetColor;
+        case 12:
+            return ELogVerbosity::Type::BreakOnLog;
+        default:
+            return static_cast<ELogVerbosity::Type>(x);
+        }
+    }
+    static void write(SerializationCtx& ctx, Buffer& buffer, ELogVerbosity::Type const& value){
+        switch (value) {
+        case ELogVerbosity::Type::VerbosityMask: {
+                buffer.write_integral<int32_t>(10);
+                return;
+        }
+        case ELogVerbosity::Type::SetColor: {
+                buffer.write_integral<int32_t>(11);
+                return;
+        }
+        case ELogVerbosity::Type::BreakOnLog: {
+                buffer.write_integral<int32_t>(12);
+                return;
+        }
+        default:
+            buffer.write_integral<int32_t>(static_cast<int32_t>(value));
+        }
+    }
 };
 
 }
