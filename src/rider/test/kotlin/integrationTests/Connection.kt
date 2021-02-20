d42b3423126b84f01dd203de9d844f3ee52510d1
Artem Chuguev <artem.chuguev@jetbrains.com>
2021-02-20 13:08:45 +0300

    autotest::added test for connection to Unreal

---
+++ src/rider/test/kotlin/integrationTests/Connection.kt
@@ -0,0 +1,54 @@
+package integrationTests
+
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rdclient.util.idea.waitAndPump
+import com.jetbrains.rider.plugins.unreal.model.frontendBackend.ForceInstall
+import com.jetbrains.rider.plugins.unreal.model.frontendBackend.InstallPluginDescription
+import com.jetbrains.rider.plugins.unreal.model.frontendBackend.PluginInstallLocation
+import com.jetbrains.rider.plugins.unreal.model.frontendBackend.rdRiderModel
+import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.withRunProgram
+import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.base.BaseTestWithSolution
+import com.jetbrains.rider.test.enums.PlatformType
+import com.jetbrains.rider.test.enums.ToolsetVersion
+import com.jetbrains.rider.test.scriptingApi.buildSolution
+import com.jetbrains.rider.test.scriptingApi.checkBuildResult
+import com.jetbrains.rider.test.scriptingApi.setConfigurationAndPlatform
+import org.testng.annotations.Test
+import java.time.Duration
+
+
+class Connection : BaseTestWithSolution() {
+
+    override val waitForCaches = true
+
+    override val backendLoadedTimeout: Duration
+        get() = Duration.ofSeconds(60)
+
+    override fun getSolutionDirectoryName(): String = "EmptyUProject"
+
+    @Test
+    @TestEnvironment(platform = [PlatformType.WINDOWS], toolset = ToolsetVersion.TOOLSET_16_CPP)
+    fun connection() {
+        // TODO move plugin installation to suite level
+        var riderLinkInstalled = false
+        project.solution.rdRiderModel.installPluginFinished.advise(Lifetime.Eternal) { riderLinkInstalled = true }
+        project.solution.rdRiderModel.installEditorPlugin.fire(InstallPluginDescription(PluginInstallLocation.Game,
+            ForceInstall.Yes))
+
+        waitAndPump(Duration.ofSeconds(90), { riderLinkInstalled })
+        waitAndPump(Duration.ofSeconds(15), { project.solution.rdRiderModel.isUnrealEngineSolution.value }, { "This is not unreal solution" })
+
+        setConfigurationAndPlatform(project, "DebugGame Editor", "Win64")
+
+        val result = buildSolution(project, timeout = Duration.ofSeconds(120))
+        checkBuildResult(result.buildResult, result.errorMessages)
+//        checkThatBuildArtifactsExist(project)  // TODO create checker for unreal projects
+
+        withRunProgram {
+            waitAndPump(Duration.ofSeconds(60),
+                { it.solution.rdRiderModel.isConnectedToUnrealEditor.value }, { "Not connected to UnrealEditor" })
+        }
+    }
+}
