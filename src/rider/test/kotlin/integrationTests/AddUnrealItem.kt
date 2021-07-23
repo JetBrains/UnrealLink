f7efbdc5a44f7140e59ba8071678f4f9a1954c6e
Artem Chuguev <artem.chuguev@jetbrains.com>
2021-07-23 12:40:18 +0300

    tmp cmt 3

--- src/rider/test/kotlin/integrationTests/AddUnrealItem.kt
+++ src/rider/test/kotlin/integrationTests/AddUnrealItem.kt
@@ -1,31 +1,25 @@
-package integrationTests
-
-import com.jetbrains.rdclient.util.idea.waitAndPump
-import com.jetbrains.rider.plugins.unreal.model.frontendBackend.rdRiderModel
-import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.UnrealTestWithSolution
-import com.jetbrains.rider.projectView.solution
-import com.jetbrains.rider.test.annotations.TestEnvironment
-import com.jetbrains.rider.test.enums.PlatformType
-import com.jetbrains.rider.test.enums.ToolsetVersion
-import com.jetbrains.rider.test.scriptingApi.buildSolution
-import com.jetbrains.rider.test.scriptingApi.checkBuildResult
-import com.jetbrains.rider.test.scriptingApi.setConfigurationAndPlatform
-import org.testng.annotations.Test
-import java.time.Duration
-
-
-@TestEnvironment(platform = [PlatformType.WINDOWS], toolset = ToolsetVersion.TOOLSET_16_CPP)
-class AddUnrealItem : UnrealTestWithSolution() {
-    init {
-        solutionName = "EmptyUProject"
-        installRiderLink = true
-        openSolutionParams.waitForCaches = true
-        openSolutionParams.backendLoadedTimeout = Duration.ofSeconds(90)
-        openSolutionParams.preprocessSolutionFile = { generateSolutionFromUProject(it) }
-    }
-
-    @Test
-    fun connection() {
-
-    }
-}
+//package integrationTests
+//
+//import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.UnrealSuiteProject
+//import com.jetbrains.rider.test.annotations.TestEnvironment
+//import com.jetbrains.rider.test.enums.PlatformType
+//import com.jetbrains.rider.test.enums.ToolsetVersion
+//import org.testng.annotations.Test
+//import java.time.Duration
+//
+//
+//@TestEnvironment(platform = [PlatformType.WINDOWS], toolset = ToolsetVersion.TOOLSET_16_CPP)
+//class AddUnrealItem : UnrealSuiteProject() {
+//    init {
+//        solutionName = "EmptyUProject"
+//        needInstallRiderLink = true
+//        openSolutionParams.waitForCaches = true
+//        openSolutionParams.backendLoadedTimeout = Duration.ofSeconds(90)
+//        openSolutionParams.preprocessSolutionFile = { generateSolutionFromUProject(it) }
+//    }
+//
+//    @Test
+//    fun connection() {
+//
+//    }
+//}
--- src/rider/test/kotlin/integrationTests/Connection.kt
+++ src/rider/test/kotlin/integrationTests/Connection.kt
@@ -1,36 +1,96 @@
 package integrationTests
 
+import com.jetbrains.rd.ide.model.UnrealEngine
 import com.jetbrains.rd.platform.util.application
-import com.jetbrains.rdclient.protocol.getComponent
 import com.jetbrains.rdclient.util.idea.waitAndPump
-import com.jetbrains.rider.cpp.unreal.UnrealShellHost
+import com.jetbrains.rider.plugins.unreal.model.frontendBackend.PluginInstallLocation
 import com.jetbrains.rider.plugins.unreal.model.frontendBackend.rdRiderModel
-import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.UnrealTestWithSolution
+import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.UnrealTestInfo
+import com.jetbrains.rider.plugins.unreal.test.testFrameworkExtentions.UnrealTestProject
 import com.jetbrains.rider.projectView.solution
-import com.jetbrains.rider.protocol.protocolHost
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.enums.ToolsetVersion
-import com.jetbrains.rider.test.scriptingApi.*
+import com.jetbrains.rider.test.framework.getLoadedProjects
+import com.jetbrains.rider.test.scriptingApi.buildSolution
+import com.jetbrains.rider.test.scriptingApi.checkBuildResult
+import com.jetbrains.rider.test.scriptingApi.setConfigurationAndPlatform
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 import java.time.Duration
 
-
 @TestEnvironment(platform = [PlatformType.WINDOWS], toolset = ToolsetVersion.TOOLSET_16_CPP)
-class Connection : UnrealTestWithSolution() {
+class Connection : UnrealTestProject() {
     init {
-        solutionName = "EmptyUProject"
-        installRiderLink = true
+        projectDirectoryName = "EmptyUProject"
         openSolutionParams.waitForCaches = true
-        openSolutionParams.backendLoadedTimeout = Duration.ofSeconds(90)
-        openSolutionParams.preprocessSolutionFile = { generateSolutionFromUProject(it) }
+        openSolutionParams.projectModelReadyTimeout = Duration.ofSeconds(150)
+        openSolutionParams.backendLoadedTimeout = Duration.ofSeconds(150)
     }
 
-    @Test
-    fun connection() {
+//    /**
+//     * @return [[]]
+//     */
+//    @DataProvider
+//    fun enginesAndOthers() = Array(unrealInfo.testingEngines.size*4)
+//        { i ->
+//            arrayOf(
+//                arrayOf(PluginInstallLocation.Game, PluginInstallLocation.Engine)[i%2],
+//                arrayOf(UnrealTestInfo.UnrealOpenType.Sln, UnrealTestInfo.UnrealOpenType.Uproject)[i%2],
+//                unrealInfo.testingEngines[i % unrealInfo.testingEngines.size]
+//            )
+//        }
+
+    @DataProvider
+    fun enginesAndOthers(): MutableIterator<Array<Any>> {
+        val result: ArrayList<Array<Any>> = arrayListOf()
+        unrealInfo.testingEngines.forEach { en ->
+            arrayOf(PluginInstallLocation.Game, PluginInstallLocation.Engine).forEach { il ->
+                arrayOf(UnrealTestInfo.UnrealOpenType.Sln, UnrealTestInfo.UnrealOpenType.Uproject).forEach { op ->
+                    result.add(arrayOf(op, il, en))
+                }
+            }
+        }
+        return result.iterator()
+    }
+
+    @Test (dataProvider = "enginesAndOthers")
+    fun connection(openWith: UnrealTestInfo.UnrealOpenType, location: PluginInstallLocation, engine: UnrealEngine) {
+//    fun connection() {
+//
+//        val location = PluginInstallLocation.Game
+//        val openWith = UnrealTestInfo.UnrealOpenType.Sln
+//        val engine = unrealInfo.testingEngines.find { it.id == "5.0EA" && it.isInstalledBuild }!!
+
+        unrealInfo.currentEngine = engine
+        unrealInfo.placeToInstallRiderLink = location
+
+        println("Test starting with $engine, RiderLink will install in $location, opening by $openWith.")
+
+        replaceUnrealEngineVersionInUproject(uprojectFile, unrealInfo.currentEngine!!)
+
+        if (openWith == UnrealTestInfo.UnrealOpenType.Sln) {
+            generateSolutionFromUProject(uprojectFile)
+            openSolutionParams.minimalCountProjectsMustBeLoaded = null
+        }
+        else {
+            openSolutionParams.minimalCountProjectsMustBeLoaded = 1400
+        }
+
+        project = openProject(openWith)
+        getLoadedProjects(project)
+
+        waitAndPump(Duration.ofSeconds(15),
+            { project.solution.rdRiderModel.isUnrealEngineSolution.value }, { "This is not unreal solution" })
+
+        if (unrealInfo.needInstallRiderLink) {
+            installRiderLink(unrealInfo.placeToInstallRiderLink)
+        }
+
         setConfigurationAndPlatform(project, "DebugGame Editor", "Win64")
 
-        buildSolutionWithReSharperBuild()
+        val result = buildSolution(project, timeout = Duration.ofSeconds(120), useIncrementalBuild = false)
+        checkBuildResult(result.buildResult, result.errorMessages)
 //        checkThatBuildArtifactsExist(project)  // TODO create checker for unreal projects
 
         withRunProgram {
