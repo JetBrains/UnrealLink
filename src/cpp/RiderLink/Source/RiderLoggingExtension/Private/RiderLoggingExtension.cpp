cc98a47c3f88ba7a558074655a605ecdf1eb9d57
Alexander Pirogov <a.g.pirogov@gmail.com>
2021-07-28 21:28:47 +0300

    Fix data race on logging messages

--- src/cpp/RiderLink/Source/RiderLoggingExtension/Private/RiderLoggingExtension.cpp
+++ src/cpp/RiderLink/Source/RiderLoggingExtension/Private/RiderLoggingExtension.cpp
@@ -16,8 +16,11 @@ DEFINE_LOG_CATEGORY(FLogRiderLoggingExtensionModule);
 
 IMPLEMENT_MODULE(FRiderLoggingExtensionModule, RiderLoggingExtension);
 
+namespace LoggingExtensionImpl
+{
 static TArray<rd::Wrapper<JetBrains::EditorPlugin::StringRange>> GetPathRanges(
-	const FRegexPattern& Pattern, const FString& Str)
+	const FRegexPattern& Pattern,
+	const FString& Str)
 {
 	using JetBrains::EditorPlugin::StringRange;
 	FRegexMatcher Matcher(Pattern, Str);
@@ -34,7 +37,8 @@ static TArray<rd::Wrapper<JetBrains::EditorPlugin::StringRange>> GetPathRanges(
 }
 
 static TArray<rd::Wrapper<JetBrains::EditorPlugin::StringRange>> GetMethodRanges(
-	const FRegexPattern& Pattern, const FString& Str)
+	const FRegexPattern& Pattern,
+	const FString& Str)
 {
 	using JetBrains::EditorPlugin::StringRange;
 	FRegexMatcher Matcher(Pattern, Str);
@@ -46,84 +50,80 @@ static TArray<rd::Wrapper<JetBrains::EditorPlugin::StringRange>> GetMethodRanges
 	return Ranges;
 }
 
+static const FRegexPattern PathPattern = FRegexPattern(TEXT("[^\\s]*/[^\\s]+"));
+static const FRegexPattern MethodPattern = FRegexPattern(TEXT("[0-9a-z_A-Z]+::~?[0-9a-z_A-Z]+"));
+
+/**
+ * @brief 
+ All data required for sending log message lives on main [game] thread.
+ FRiderLoggingExtensionModule depends on FRiderLinkModule, so we're safe to get handler here.
+ The only point of sync is RdEditorModel, so we wrap it in IRiderLinkModule::FireAsyncAction.
+ */
+static bool SendMessageToRider(const JetBrains::EditorPlugin::LogMessageInfo& MessageInfo, const FString& Message)
+{
+	return IRiderLinkModule::Get().FireAsyncAction(
+[&MessageInfo, &Message]
+		(JetBrains::EditorPlugin::RdEditorModel const& RdEditorModel)
+		{
+			rd::ISignal<JetBrains::EditorPlugin::UnrealLogEvent> const& UnrealLog = RdEditorModel.get_unrealLog();
+			UnrealLog.fire({
+				MessageInfo,
+				Message,
+				GetPathRanges(PathPattern, Message),
+				GetMethodRanges(MethodPattern, Message)
+			});
+		}
+	);
+}	
+}
+
+
 void FRiderLoggingExtensionModule::StartupModule()
 {
 	UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("STARTUP START"));
 
-	static const auto START_TIME = FDateTime::UtcNow();
+	static const auto START_TIME = FDateTime::UtcNow().ToUnixTimestamp();
 	static const auto GetTimeNow = [](double Time) -> rd::DateTime
 	{
-		return rd::DateTime(static_cast<std::time_t>(START_TIME.ToUnixTimestamp() +
-			static_cast<int64>(Time)));
+		return rd::DateTime(START_TIME + static_cast<int64>(Time));
 	};
 
-	IRiderLinkModule& RiderLinkModule = IRiderLinkModule::Get();
-	ModuleLifetimeDef = RiderLinkModule.CreateNestedLifetimeDefinition();
-	RiderLinkModule.ViewModel(ModuleLifetimeDef.lifetime,
-	[this](rd::Lifetime ModelLifetime, JetBrains::EditorPlugin::RdEditorModel const& RdEditorModel)
-    {
-        ModelLifetime->bracket(
-        [this, &RdEditorModel]()
-       {
-           outputDevice.onSerializeMessage.BindLambda(
-               [this, &RdEditorModel](
-               const TCHAR* msg, ELogVerbosity::Type Type,
-               const class FName& Name, TOptional<double> Time)
-               {
-                   if (Type > ELogVerbosity::All) return;
+	// Even though we don't use LifetimeDefinition here, calling to IRiderLinkModule::Get() ensures that RiderLinkModule
+	// will be initialized before FRiderLoggingExtensionModule, so we'll be safe to call it in output device handler.
+	ModuleLifetimeDef = IRiderLinkModule::Get().CreateNestedLifetimeDefinition();
+	
+	// Subscribe on main [game] thread
+	outputDevice.onSerializeMessage.BindLambda(
+		// Log is called on main [game] thread
+		[] ( const TCHAR* msg, ELogVerbosity::Type Type, const class FName& Name, TOptional<double> Time)
+		{
+			if (Type > ELogVerbosity::All) return;
 
-                   rd::ISignal<
-                           JetBrains::EditorPlugin::UnrealLogEvent>
-                       const& UnrealLog = RdEditorModel.
-                           get_unrealLog();
-                   rd::optional<rd::DateTime> DateTime;
-                   if (Time)
-                   {
-                       DateTime = GetTimeNow(Time.GetValue());
-                   }
-                   const FString Msg = FString(msg);
-                   const FString PlainName = Name.
-                       GetPlainNameString();
+			rd::optional<rd::DateTime> DateTime;
+			if (Time)
+			{
+				DateTime = GetTimeNow(Time.GetValue());
+			}
+			const FString Msg = FString(msg);
+			const FString PlainName = Name.GetPlainNameString();
 
-                   JetBrains::EditorPlugin::LogMessageInfo
-                       MessageInfo{Type, PlainName, DateTime};
+			const JetBrains::EditorPlugin::LogMessageInfo MessageInfo{Type, PlainName, DateTime};
 
-                   // [HACK]: fix https://github.com/JetBrains/UnrealLink/issues/17
-                   // while we won't change BP hyperlink parsing
-                   FString Tail = Msg.Left(4096);
+			// [HACK]: fix https://github.com/JetBrains/UnrealLink/issues/17
+			// while we won't change BP hyperlink parsing
+			FString Tail = Msg.Left(4096);
 
-                   const FRegexPattern PathPattern = FRegexPattern(
-                       TEXT("[^\\s]*/[^\\s]+"));
-                   const FRegexPattern MethodPattern =
-                       FRegexPattern(
-                           TEXT("[0-9a-z_A-Z]+::~?[0-9a-z_A-Z]+"));
-                   FString ToSend;
-                   while (Tail.Split("\n", &ToSend, &Tail))
-                   {
-                       ToSend.TrimEndInline();
-                       UnrealLog.fire(
-                           JetBrains::EditorPlugin::UnrealLogEvent{
-                               MessageInfo, ToSend,
-                               GetPathRanges(PathPattern, ToSend),
-                               GetMethodRanges(
-                                   MethodPattern, ToSend)
-                           });
-                   }
-                   Tail.TrimEndInline();
-                   UnrealLog.fire(
-                       JetBrains::EditorPlugin::UnrealLogEvent{
-                           MessageInfo, Tail,
-                           GetPathRanges(PathPattern, Tail),
-                           GetMethodRanges(MethodPattern, Tail)
-                       });
-               });
-       },
-       [this]()
-       {
-			if (outputDevice.onSerializeMessage.IsBound())
-			    outputDevice.onSerializeMessage.Unbind();
-       });
-    });
+			FString ToSend;
+			while (Tail.Split("\n", &ToSend, &Tail))
+			{
+				ToSend.TrimEndInline();
+				LoggingExtensionImpl::SendMessageToRider(MessageInfo, ToSend);
+			}
+			
+			Tail.TrimEndInline();            
+			LoggingExtensionImpl::SendMessageToRider(MessageInfo, Tail);
+        }
+    );
 
 	UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("STARTUP FINISH"));
 }
@@ -131,6 +131,10 @@ void FRiderLoggingExtensionModule::StartupModule()
 void FRiderLoggingExtensionModule::ShutdownModule()
 {
 	UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("SHUTDOWN START"));
+	// FRiderLoggingExtensionModule::ShutdownModule() before FRiderLinkModule::Shutdown()
+	// RiderLinkModule will be still valid in output device handler
+	if (outputDevice.onSerializeMessage.IsBound())
+		outputDevice.onSerializeMessage.Unbind();
 	ModuleLifetimeDef.terminate();
 	UE_LOG(FLogRiderLoggingExtensionModule, Verbose, TEXT("SHUTDOWN FINISH"));
 }
