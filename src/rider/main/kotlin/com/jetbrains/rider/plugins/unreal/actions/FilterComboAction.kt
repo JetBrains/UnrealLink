2da21eb848c05b91add3ab4806b0a378dc5241da
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2021-11-30 15:34:06 +0300

    Do not close verbosity and category popups on checkbox toggle

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/actions/FilterComboAction.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/actions/FilterComboAction.kt
@@ -1,22 +1,27 @@
 package com.jetbrains.rider.plugins.unreal.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.ex.CheckboxAction
-import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.ui.popup.KeepingPopupOpenAction
+import com.jetbrains.rdclient.usages.SelfUpdatingCheckIcon
 import kotlin.reflect.KMutableProperty0
 
 class FilterCheckboxAction(
         text: String,
         private val isSelected: () -> Boolean,
         private val setSelected: (Boolean) -> Unit
-) : CheckboxAction(text), DumbAware {
+) : DumbAwareAction(text), KeepingPopupOpenAction {
     constructor(text: String, property: KMutableProperty0<Boolean>) : this(text, property::get, property::set)
 
-    override fun isSelected(e: AnActionEvent): Boolean {
-        return isSelected()
+    private val checkboxIcon = SelfUpdatingCheckIcon(12) { isSelected() }
+
+    init {
+        templatePresentation.icon = checkboxIcon
+        templatePresentation.selectedIcon = checkboxIcon
     }
 
-    override fun setSelected(e: AnActionEvent, state: Boolean) {
-        setSelected(state)
+    override fun actionPerformed(e: AnActionEvent) {
+        val curState = isSelected()
+        setSelected(!curState)
     }
 }
