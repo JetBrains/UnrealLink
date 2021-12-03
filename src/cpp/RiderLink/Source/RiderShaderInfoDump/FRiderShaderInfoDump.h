e7c3b8c9359dae78d96d8f3404559ab720a4defd
Alexander Pirogov <a.g.pirogov@gmail.com>
2021-12-03 16:59:06 +0300

    Dump shader virtual file mappings

---
+++ src/cpp/RiderLink/Source/RiderShaderInfoDump/FRiderShaderInfoDump.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "Modules/ModuleInterface.h"
+
+class RIDERSHADERINFODUMP_API FRiderShaderInfoDump : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+};
---
+++ src/cpp/RiderLink/Source/RiderShaderInfoDump/RiderShaderInfoDump.Build.cs
@@ -0,0 +1,11 @@
+﻿// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RiderShaderInfoDump : ModuleRules
+{
+	public RiderShaderInfoDump(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.AddRange(new string[] { "Core",  "Projects", "RenderCore" });
+	}
+}
