cd53b0d88387b8d099b8846de00c9d5676628545
Mikhail Filippov <mikhail@filippov.me>
2022-01-14 15:22:25 +0300

    Add plugin snapshots repo.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
+        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
     }
     resolutionStrategy {
         eachPlugin {
