f45064642c081f00682b4743e0792795d763f20b
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2022-02-22 01:14:01 +0300

    RIDER-71784: Add unreal log presentation options: * show/hide log verbosity type * align log messages

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/toolWindow/log/UnrealLogPanelSettings.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/toolWindow/log/UnrealLogPanelSettings.kt
@@ -9,11 +9,6 @@ class UnrealLogPanelSettings(private val project: Project) : SimplePersistentSta
         fun getInstance(project: Project): UnrealLogPanelSettings = project.service()
     }
 
-    var showTimestamps: Boolean
-        get() = state.showTimestamps
-        set(value) {
-            state.showTimestamps = value
-        }
     var showMessages: Boolean
         get() = state.showMessages
         set(value) {
@@ -35,13 +30,37 @@ class UnrealLogPanelSettings(private val project: Project) : SimplePersistentSta
             state.showAllCategories = value
         }
 
-    class State : BaseState() {
-        var showTimestamps by property(defaultValue = false)
+    var showTimestamps: Boolean
+        get() = state.showTimestamps
+        set(value) {
+            state.showTimestamps = value
+        }
+    var showVerbosity: Boolean
+        get() = state.showVerbosity
+        set(value) {
+            state.showVerbosity = value
+        }
+    var alignMessages: Boolean
+        get() = state.alignMessages
+        set(value) {
+            state.alignMessages = value
+        }
+    var categoryWidth: Int
+        get() = state.categoryWidth
+        set(value) {
+            state.categoryWidth = value
+        }
 
+    class State : BaseState() {
         var showMessages by property(defaultValue = true)
         var showWarnings by property(defaultValue = true)
         var showErrors by property(defaultValue = true)
         var showAllCategories by property(defaultValue = true)
+
+        var showTimestamps by property(defaultValue = false)
+        var showVerbosity by property(defaultValue = true)
+        var alignMessages by property(defaultValue = true)
+        var categoryWidth by property(defaultValue = 25)
     }
 }
 
