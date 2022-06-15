61840e624d20cfcbf00e87d353b63e5ca0ab6e12
Artem Chuguev <artem.chuguev@jetbrains.com>
2022-06-15 19:55:18 +0200

    Added new test solution "TestPuzzleProject"

---
+++ testData/solutions/TestPuzzleProject/Source/TestPuzzleProject/TestPuzzleProjectBlock.cpp
@@ -0,0 +1,94 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestPuzzleProjectBlock.h"
+#include "TestPuzzleProjectBlockGrid.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Materials/MaterialInstance.h"
+
+ATestPuzzleProjectBlock::ATestPuzzleProjectBlock()
+{
+	// Structure to hold one-time initialization
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> PlaneMesh;
+		ConstructorHelpers::FObjectFinderOptional<UMaterial> BaseMaterial;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> BlueMaterial;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> OrangeMaterial;
+		FConstructorStatics()
+			: PlaneMesh(TEXT("/Game/Puzzle/Meshes/PuzzleCube.PuzzleCube"))
+			, BaseMaterial(TEXT("/Game/Puzzle/Meshes/BaseMaterial.BaseMaterial"))
+			, BlueMaterial(TEXT("/Game/Puzzle/Meshes/BlueMaterial.BlueMaterial"))
+			, OrangeMaterial(TEXT("/Game/Puzzle/Meshes/OrangeMaterial.OrangeMaterial"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+
+	// Create dummy root scene component
+	DummyRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Dummy0"));
+	RootComponent = DummyRoot;
+
+	// Create static mesh component
+	BlockMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BlockMesh0"));
+	BlockMesh->SetStaticMesh(ConstructorStatics.PlaneMesh.Get());
+	BlockMesh->SetRelativeScale3D(FVector(1.f,1.f,0.25f));
+	BlockMesh->SetRelativeLocation(FVector(0.f,0.f,25.f));
+	BlockMesh->SetMaterial(0, ConstructorStatics.BlueMaterial.Get());
+	BlockMesh->SetupAttachment(DummyRoot);
+	BlockMesh->OnClicked.AddDynamic(this, &ATestPuzzleProjectBlock::BlockClicked);
+	BlockMesh->OnInputTouchBegin.AddDynamic(this, &ATestPuzzleProjectBlock::OnFingerPressedBlock);
+
+	// Save a pointer to the orange material
+	BaseMaterial = ConstructorStatics.BaseMaterial.Get();
+	BlueMaterial = ConstructorStatics.BlueMaterial.Get();
+	OrangeMaterial = ConstructorStatics.OrangeMaterial.Get();
+}
+
+void ATestPuzzleProjectBlock::BlockClicked(UPrimitiveComponent* ClickedComp, FKey ButtonClicked)
+{
+	HandleClicked();
+}
+
+
+void ATestPuzzleProjectBlock::OnFingerPressedBlock(ETouchIndex::Type FingerIndex, UPrimitiveComponent* TouchedComponent)
+{
+	HandleClicked();
+}
+
+void ATestPuzzleProjectBlock::HandleClicked()
+{
+	// Check we are not already active
+	if (!bIsActive)
+	{
+		bIsActive = true;
+
+		// Change material
+		BlockMesh->SetMaterial(0, OrangeMaterial);
+
+		// Tell the Grid
+		if (OwningGrid != nullptr)
+		{
+			OwningGrid->AddScore();
+		}
+	}
+}
+
+void ATestPuzzleProjectBlock::Highlight(bool bOn)
+{
+	// Do not highlight if the block has already been activated.
+	if (bIsActive)
+	{
+		return;
+	}
+
+	if (bOn)
+	{
+		BlockMesh->SetMaterial(0, BaseMaterial);
+	}
+	else
+	{
+		BlockMesh->SetMaterial(0, BlueMaterial);
+	}
+}
---
+++ testData/solutions/TestPuzzleProject/Source/TestPuzzleProject/TestPuzzleProjectPlayerController.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestPuzzleProjectPlayerController.h"
+
+ATestPuzzleProjectPlayerController::ATestPuzzleProjectPlayerController()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableTouchEvents = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
