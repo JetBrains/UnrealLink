047d6fb1709491fc13b03fbadd4d728bbf6246aa
Artem Chuguev <artem.chuguev@jetbrains.com>
2022-06-15 19:55:18 +0200

    Added new test solution "TestPuzzleProject"

---
+++ testData/solutions/TestPuzzleProject/Source/TestPuzzleProject/TestPuzzleProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TestPuzzleProject : ModuleRules
+{
+	public TestPuzzleProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
