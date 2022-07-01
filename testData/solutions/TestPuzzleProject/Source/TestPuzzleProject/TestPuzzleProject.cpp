0385de5ec2e1f73b683f3b95ae70bd8187f43ef7
Artem Chuguev <artem.chuguev@jetbrains.com>
2022-07-01 13:17:57 +0200

    Remove TestPuzzleProject from test data

--- testData/solutions/TestPuzzleProject/Source/TestPuzzleProject/TestPuzzleProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestPuzzleProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, TestPuzzleProject, "TestPuzzleProject");
