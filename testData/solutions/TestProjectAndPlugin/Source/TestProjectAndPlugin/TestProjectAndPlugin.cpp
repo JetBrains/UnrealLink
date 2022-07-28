c7846a37e6b20b83b49a7c68c5fa344bec581c88
Vasiliy Dommes <vasiliy.dommes@jetbrains.com>
2022-07-28 18:13:54 +0300

    Add test for rename in game plugin with core redirects
    TODO test the case when Config folder is absent
    TODO test engine plugins

---
+++ testData/solutions/TestProjectAndPlugin/Source/TestProjectAndPlugin/TestProjectAndPlugin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestProjectAndPlugin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestProjectAndPlugin, "TestProjectAndPlugin" );
