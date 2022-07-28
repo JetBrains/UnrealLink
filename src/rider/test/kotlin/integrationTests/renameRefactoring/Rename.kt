084a836ec298aaf210351a7aa623060f31349df4
Vasiliy Dommes <vasiliy.dommes@jetbrains.com>
2022-07-28 18:13:54 +0300

    Add test for rename in game plugin with core redirects
    TODO test the case when Config folder is absent
    TODO test engine plugins

--- src/rider/test/kotlin/integrationTests/renameRefactoring/Rename.kt
+++ src/rider/test/kotlin/integrationTests/renameRefactoring/Rename.kt
@@ -12,17 +12,16 @@ import com.jetbrains.rd.ui.bedsl.extensions.tryGetBeControlById
 import com.jetbrains.rider.actions.RiderActions
 import com.jetbrains.rider.model.refactorings.BeRefactoringsPage
 import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.asserts.shouldNotBeNull
 import com.jetbrains.rider.test.enums.CoreVersion
 import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.enums.ToolsetVersion
-import com.jetbrains.rider.test.framework.combine
 import com.jetbrains.rider.test.framework.executeWithGold
 import com.jetbrains.rider.test.framework.frameworkLogger
 import com.jetbrains.rider.test.framework.waitBackendAndWorkspaceModel
 import com.jetbrains.rider.test.scriptingApi.*
 import org.apache.commons.io.FileUtils
 import org.testng.Assert
-import org.testng.annotations.BeforeMethod
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 import testFrameworkExtentions.EngineInfo
@@ -36,22 +35,11 @@ import java.time.Duration
 )
 class Rename : UnrealTestProject() {
     init {
-        projectDirectoryName = "EmptyUProject"
+        projectDirectoryName = "TestProjectAndPlugin"
         openSolutionParams.waitForCaches = true
         openSolutionParams.projectModelReadyTimeout = Duration.ofSeconds(150)
         openSolutionParams.backendLoadedTimeout = Duration.ofSeconds(150)
         openSolutionParams.initWithCachesTimeout = Duration.ofSeconds(120)
-    }
-
-    @BeforeMethod
-    override fun testSetup() {
-        super.testSetup()
-        testDirectory.combine("Source").copyRecursively(
-            activeSolutionDirectory.resolve("Source").resolve(projectDirectoryName)
-        )
-        testDirectory.combine("Config").copyRecursively(
-            activeSolutionDirectory.resolve("Config")
-        )
     }
 
     @Test(dataProvider = "enginesAndOthers")
@@ -63,12 +51,27 @@ class Rename : UnrealTestProject() {
         unrealInTestSetup(openWith, engine)
         project = openProject(openWith)
 
-        val sourceFilePath = activeSolutionDirectory.combine("Source", projectDirectoryName, "MyActor.h").path
-        val symbolsToRename = arrayOf("bMyProperty", "AMyActor", "bMyPropertyRename", "AMyActorRename")
-        val virtualFile = renameSymbolsInFile(sourceFilePath, symbolsToRename)
+        val virtualFiles = arrayListOf<VirtualFile>()
+
+        val classNameAndPaths = arrayOf(
+            Pair("AMyActor", activeSolutionDirectory / "Source" / projectDirectoryName / "MyActor.h"),
+            Pair(
+                "AMyPluginActor",
+                activeSolutionDirectory / "Plugins" / "TestPlugin" / "Source" / "TestPlugin" / "Public" / "MyPluginActor.h"
+            )
+        )
+
+        for ((className, path) in classNameAndPaths) {
+            val propertyName = "bMyProperty"
+            val symbolsToRename = arrayOf(propertyName, className, propertyName + "Rename", className + "Rename")
+            val virtualFile = renameSymbolsInFile(path.path, symbolsToRename)
+            virtualFiles.add(virtualFile.shouldNotBeNull("virtualFile == null for $path"))
+        }
 
         executeWithGold(testGoldFile) {
-            dumpFile(virtualFile!!, it)
+            for (file in virtualFiles) {
+                dumpFile(file, it)
+            }
             val iniFiles = FileUtils.listFiles(activeSolutionDirectory, arrayOf("ini"), true)
             for (file in iniFiles) {
                 val iniFile = getVirtualFileFromPath(file.path)
@@ -83,7 +86,7 @@ class Rename : UnrealTestProject() {
         it.println(relPath)
         it.println("---")
         val document = FileDocumentManager.getInstance().getDocument(file)
-        it.println(document!!.text)
+        it.println(document.shouldNotBeNull("Cannot get document for $file").text)
     }
 
     private fun renameSymbolsInFile(path: String, symbolsToRename: Array<String>): VirtualFile? {
@@ -116,10 +119,12 @@ class Rename : UnrealTestProject() {
                         val renameFiles = content.tryGetBeControlById<BeCheckbox>("Rename.RenameFile")
                         renameFiles?.property?.set(false) // TODO fix exceptions and set(true)
                     }
+
                     1 -> {
                         val coreRedirect = content.getBeControlById<BeCheckbox>("ShouldCoreRedirect")
                         coreRedirect.property.set(true)
                     }
+
                     else -> {
                         Assert.fail("Unexpected page $page")
                     }
