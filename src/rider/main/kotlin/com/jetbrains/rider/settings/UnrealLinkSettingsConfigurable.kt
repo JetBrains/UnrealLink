41b67f783196d5e1a6df2ee058793475e19d9d84
Alexander Pirogov <alexander.pirogov@jetbrains.com>
2023-01-05 18:57:34 +0200

    Fix https://youtrack.jetbrains.com/issue/RIDER-82232/Not-localized-File-Settings-Languages-Frameworks-Unreal-Engine

--- src/rider/main/kotlin/com/jetbrains/rider/settings/UnrealLinkSettingsConfigurable.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/settings/UnrealLinkSettingsConfigurable.kt
@@ -1,7 +1,8 @@
 package com.jetbrains.rider.settings
 
+import com.jetbrains.rider.UnrealLinkBundle
 import com.jetbrains.rider.settings.simple.SimpleOptionsPage
 
-class UnrealLinkSettingsConfigurable : SimpleOptionsPage("Unreal Engine", "UnrealLinkOptions") {
+class UnrealLinkSettingsConfigurable : SimpleOptionsPage(UnrealLinkBundle.message("configurable.UnrealLink.settings.title"), "UnrealLinkOptions") {
     override fun getId() = pageId + "Id"
 }
