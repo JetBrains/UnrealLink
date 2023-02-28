da2866e6441870637c3591952e60076556180ceb
Alexander Pirogov <a.g.pirogov@gmail.com>
2023-02-28 18:32:08 +0200

    Fix RIDER-90283 Moving LiveCoding support into a separate module and allowing only Win64 platform, 'cause LC doesn't work anywhere else

---
+++ src/cpp/RiderLink/Source/RiderLC/Private/RiderLC.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "lifetime/LifetimeDefinition.h"
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(FLogRiderLCModule, Log, All);
+
+class FRiderLCModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+	virtual bool SupportsDynamicReloading() override { return true; }
+	void SetupLiveCodingBinds();
+	
+private:
+    FDelegateHandle PatchCompleteHandle;
+    rd::LifetimeDefinition ModuleLifetimeDef;
+};
