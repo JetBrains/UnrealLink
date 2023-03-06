2aa424d8d4ab8b9597785747d29d55cc5ca3eb39
Artem.Chuguev <artem.chuguev@jetbrains.com>
2023-03-06 11:47:24 +0100

    [Autotests] Update gold

---
+++ testData/solutions/BlueprintCodeVision/Source/BlueprintCodeVision/BlueprintCodeVisionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlueprintCodeVisionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLUEPRINTCODEVISION_API ABlueprintCodeVisionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
