c45ff27f294514a6a464105d33c0116459cfa636
denis.kuznetsov <denis.kuznetsov@jetbrains.com>
2023-03-07 17:34:18 +0100

    fix language unsupported issues

--- src/dotnet/RiderPlugin.UnrealLink/LiveCoding/LiveCodingInteractionProvider.cs
+++ src/dotnet/RiderPlugin.UnrealLink/LiveCoding/LiveCodingInteractionProvider.cs
@@ -5,29 +5,41 @@ using JetBrains.ProjectModel;
 using JetBrains.Rd.Tasks;
 using JetBrains.ReSharper.Feature.Services.Cpp.UE4.LiveCoding;
 
-namespace RiderPlugin.UnrealLink.LiveCoding;
-
-[SolutionComponent]
-public class LiveCodingInteractionProvider : ILiveCodingProvider
+namespace RiderPlugin.UnrealLink.LiveCoding
 {
+
+  [SolutionComponent]
+  public class LiveCodingInteractionProvider : ILiveCodingProvider
+  {
     private readonly RiderBackendToUnrealEditor _riderToEditor;
     private readonly LifetimeDefinition _lifetimeDef;
 
     public LiveCodingInteractionProvider(RiderBackendToUnrealEditor RiderToEditor)
     {
-        _riderToEditor = RiderToEditor;
-        _lifetimeDef = RiderToEditor.NestedLifetime();
+      _riderToEditor = RiderToEditor;
+      _lifetimeDef = RiderToEditor.NestedLifetime();
     }
 
-    public IRdTask<bool> HasStarted() => _riderToEditor.EditorModel.LC_HasStarted.Start(_lifetimeDef.Lifetime, Unit.Instance);
-    public IRdTask<bool> IsCompiling() => _riderToEditor.EditorModel.LC_IsCompiling.Start(_lifetimeDef.Lifetime, Unit.Instance);
-    public IRdTask<bool> CanEnableForSession() => _riderToEditor.EditorModel.LC_CanEnableForSession.Start(_lifetimeDef.Lifetime, Unit.Instance);
-    public IRdTask<bool> IsEnabledForSession() => _riderToEditor.EditorModel.LC_IsEnabledForSession.Start(_lifetimeDef.Lifetime, Unit.Instance);
-    public IRdTask<bool> IsEnabledByDefault() => _riderToEditor.EditorModel.LC_IsEnabledByDefault.Start(_lifetimeDef.Lifetime, Unit.Instance);
+    public IRdTask<bool> HasStarted() =>
+      _riderToEditor.EditorModel.LC_HasStarted.Start(_lifetimeDef.Lifetime, Unit.Instance);
+
+    public IRdTask<bool> IsCompiling() =>
+      _riderToEditor.EditorModel.LC_IsCompiling.Start(_lifetimeDef.Lifetime, Unit.Instance);
+
+    public IRdTask<bool> CanEnableForSession() =>
+      _riderToEditor.EditorModel.LC_CanEnableForSession.Start(_lifetimeDef.Lifetime, Unit.Instance);
+
+    public IRdTask<bool> IsEnabledForSession() =>
+      _riderToEditor.EditorModel.LC_IsEnabledForSession.Start(_lifetimeDef.Lifetime, Unit.Instance);
+
+    public IRdTask<bool> IsEnabledByDefault() =>
+      _riderToEditor.EditorModel.LC_IsEnabledByDefault.Start(_lifetimeDef.Lifetime, Unit.Instance);
+
     public void Compile() => _riderToEditor.EditorModel.LC_Compile();
     public void EnableByDefault(bool enable) => _riderToEditor.EditorModel.LC_EnableByDefault(enable);
     public void EnableForSession(bool enable) => _riderToEditor.EditorModel.LC_EnableForSession(enable);
 
     public void SubscribeOnPatchComplete(Action<Unit> handle) =>
-        _riderToEditor.EditorModel.LC_OnPatchComplete.Advise(_lifetimeDef.Lifetime, handle);
+      _riderToEditor.EditorModel.LC_OnPatchComplete.Advise(_lifetimeDef.Lifetime, handle);
+  }
 }
