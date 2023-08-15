849be1810ad6aadb8c5a9048e195d0212f70987e
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-08-15 18:47:33 +0200

    UnrealLink: rename Icons to icons

--- src/rider/main/kotlin/icons/UnrealIcons.kt
+++ src/rider/main/kotlin/icons/UnrealIcons.kt
@@ -6,25 +6,25 @@ class UnrealIcons {
     class ConnectionStatus {
         companion object {
             // TODO: add proper icon for settings
-            val UnrealEngineLogo = IconLoader.getIcon("/Icons/toolWindows/toolWindowUnrealLog.svg", UnrealIcons::class.java)
-            val UnrealEngineConnected = IconLoader.getIcon("/Icons/run/unrealEngineConnected.svg", UnrealIcons::class.java)
-            val UnrealEngineDisconnected = IconLoader.getIcon("/Icons/run/unrealEngineDisconnected.svg", UnrealIcons::class.java)
+            val UnrealEngineLogo = IconLoader.getIcon("/icons/toolWindows/toolWindowUnrealLog.svg", UnrealIcons::class.java)
+            val UnrealEngineConnected = IconLoader.getIcon("/icons/run/unrealEngineConnected.svg", UnrealIcons::class.java)
+            val UnrealEngineDisconnected = IconLoader.getIcon("/icons/run/unrealEngineDisconnected.svg", UnrealIcons::class.java)
         }
     }
 
     class PIEControl {
         companion object {
-            val Pause = IconLoader.getIcon("/Icons/run/unrealStatusPause.svg", UnrealIcons::class.java)
-            val Play = IconLoader.getIcon("/Icons/run/unrealStatusPlay.svg", UnrealIcons::class.java)
-            val Stop = IconLoader.getIcon("/Icons/run/unrealStatusStop.svg", UnrealIcons::class.java)
-            val FrameSkip = IconLoader.getIcon("/Icons/run/unrealFrameSkip.svg", UnrealIcons::class.java)
+            val Pause = IconLoader.getIcon("/icons/run/unrealStatusPause.svg", UnrealIcons::class.java)
+            val Play = IconLoader.getIcon("/icons/run/unrealStatusPlay.svg", UnrealIcons::class.java)
+            val Stop = IconLoader.getIcon("/icons/run/unrealStatusStop.svg", UnrealIcons::class.java)
+            val FrameSkip = IconLoader.getIcon("/icons/run/unrealFrameSkip.svg", UnrealIcons::class.java)
 
         }
     }
 
     class ToolWindow {
         companion object {
-            val UnrealToolWindow = IconLoader.getIcon("/Icons/toolWindows/toolWindowUnrealLog.svg", UnrealIcons::class.java)
+            val UnrealToolWindow = IconLoader.getIcon("/icons/toolWindows/toolWindowUnrealLog.svg", UnrealIcons::class.java)
         }
     }
 }
