b1e338ff992b7ee20534e4ec43681b98c63ea447
Aleksandr Pirogov <alexander.pirogov@jetbrains.com>
2023-11-14 13:28:07 +0200

    Fix building plugin on version of UE < 5.3
    (cherry picked from commit 4f322e66f8aef113041b36bf842b3edada1de9ec)

--- src/cpp/RiderLink/Source/RiderLogging/Private/RiderOutputDevice.hpp
+++ src/cpp/RiderLink/Source/RiderLogging/Private/RiderOutputDevice.hpp
@@ -3,8 +3,14 @@
 #include "Misc/OutputDevice.h"
 #include "Delegates/Delegate.h"
 #include "Logging/LogVerbosity.h"
+#include "Runtime/Launch/Resources/Version.h"
 
-using FOnSerializeMessage = TDelegate<void(const TCHAR*, ELogVerbosity::Type, const FName&, TOptional<double>), FDefaultTSDelegateUserPolicy>;
+using FOnSerializeMessage =
+#if ENGINE_MAJOR_VERSION < 5 || (ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 3)
+	TDelegate<void(const TCHAR*, ELogVerbosity::Type, const FName&, TOptional<double>)>;
+#else
+	TDelegate<void(const TCHAR*, ELogVerbosity::Type, const FName&, TOptional<double>), FDefaultTSDelegateUserPolicy>;
+#endif
 
 class FRiderOutputDevice : public FOutputDevice {
 public:
