89a3d5f9f6123eb38f80e83083b66b39977964e8
Aleksei Gusarov <Aleksei.Gusarov@jetbrains.com>
2024-03-25 13:43:09 +0100

    RIDER-107412 added a more descriptive title to the stack frame
    GitOrigin-RevId: 1af6e4acf6c64c9d77d4d7ff2ed5ce3f9aea9772

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/debugger/frames/BlueprintFrameMessages.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/debugger/frames/BlueprintFrameMessages.kt
@@ -2,4 +2,4 @@ package com.jetbrains.rider.plugins.unreal.debugger.frames
 
 import com.jetbrains.rider.UnrealLinkBundle
 
-val blueprintTitlePrefix = "[${UnrealLinkBundle.message("RiderLink.Unreal.Debugger.BlueprintCallstack.Blueprint.title")}]"
+val blueprintTitlePrefix = "[${UnrealLinkBundle.message("RiderLink.Unreal.Debugger.BlueprintCallstack.Blueprint.title")}] "
