2d131d313f7ce9e178479d718b548f913b60e32f
Aleksandr Pirogov <alexander.pirogov@jetbrains.com>
2024-04-08 20:48:36 +0300

    Migrate OutOfSyncEditorNotification.kt to protocol listeners instead of component system
    GitOrigin-RevId: a915488b451417ca0e4839e8dc717f9b1385df21

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/notifications/OutOfSyncPluginProtocolListener.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unreal/notifications/OutOfSyncPluginProtocolListener.kt
@@ -1,20 +1,20 @@
 package com.jetbrains.rider.plugins.unreal.notifications
 
 import com.intellij.notification.*
-import com.intellij.openapi.project.Project
-import com.jetbrains.rd.platform.util.idea.ProtocolSubscribedProjectComponent
+import com.intellij.openapi.client.ClientProjectSession
+import com.jetbrains.rd.protocol.SolutionExtListener
+import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.reactive.adviseNotNull
 import com.jetbrains.rider.UnrealLinkBundle
 import com.jetbrains.rider.plugins.unreal.model.frontendBackend.*
-import com.jetbrains.rider.projectView.solution
 
-class OutOfSyncEditorNotification(project: Project) : ProtocolSubscribedProjectComponent(project) {
+class OutOfSyncPluginProtocolListener : SolutionExtListener<RdRiderModel> {
     companion object {
         private const val OUT_OF_SYNC_NOTIFICATION_GROUP_ID = "OutOfSyncConnection"
     }
 
-    init {
-        project.solution.rdRiderModel.onEditorPluginOutOfSync.adviseNotNull(projectComponentLifetime) {
+    override fun extensionCreated(lifetime: Lifetime, session: ClientProjectSession, model: RdRiderModel) {
+        model.onEditorPluginOutOfSync.adviseNotNull(lifetime) {
             if (it.status == PluginInstallStatus.UpToDate) return@adviseNotNull
 
             val message = if (it.status == PluginInstallStatus.NoPlugin)
@@ -30,21 +30,21 @@ class OutOfSyncEditorNotification(project: Project) : ProtocolSubscribedProjectC
                 UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.title.wrongVersion")
 
             val notification = NotificationGroupManager.getInstance()
-                    .getNotificationGroup(OUT_OF_SYNC_NOTIFICATION_GROUP_ID)
-                    .createNotification(title, message, NotificationType.WARNING)
+                .getNotificationGroup(OUT_OF_SYNC_NOTIFICATION_GROUP_ID)
+                .createNotification(title, message, NotificationType.WARNING)
 
             when (it.status) {
                 PluginInstallStatus.NoPlugin -> {
                     notification.apply {
                         addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInEngine.text")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Engine, ForceInstall.No)
                             )
                         })
                         if (it.isGameAvailable) addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInGame.text")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Game, ForceInstall.No)
                             )
                         })
@@ -54,13 +54,13 @@ class OutOfSyncEditorNotification(project: Project) : ProtocolSubscribedProjectC
                     notification.apply {
                         addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInEngine.text.update")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Engine, ForceInstall.No)
                             )
                         })
                         if (it.isGameAvailable) addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInGame.text.move")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Game, ForceInstall.No)
                             )
                         })
@@ -70,13 +70,13 @@ class OutOfSyncEditorNotification(project: Project) : ProtocolSubscribedProjectC
                     notification.apply {
                         addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInEngine.text.move")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Engine, ForceInstall.No)
                             )
                         })
                         if (it.isGameAvailable) addAction(NotificationAction.createSimple(UnrealLinkBundle.message("notificationAction.UnrealEditorOutOfSync.installPluginInGame.text.update")) {
                             expire()
-                            project.solution.rdRiderModel.installEditorPlugin.fire(
+                            model.installEditorPlugin.fire(
                                 InstallPluginDescription(PluginInstallLocation.Game, ForceInstall.No)
                             )
                         })
@@ -85,7 +85,7 @@ class OutOfSyncEditorNotification(project: Project) : ProtocolSubscribedProjectC
                 else -> return@adviseNotNull
             }
 
-            Notifications.Bus.notify(notification, project)
+            Notifications.Bus.notify(notification, session.project)
         }
     }
 }
