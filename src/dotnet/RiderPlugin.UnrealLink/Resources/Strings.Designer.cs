51f5315da010fbd3d948850b321f4ff0db3d9fe1
Slava Trenogin <slava.trenogin@jetbrains.com>
2024-05-13 19:22:50 +0200

    Update JetResourceGenerator for C#
    GitOrigin-RevId: 8b3332491776c284aa675550fc02855873083df6

--- src/dotnet/RiderPlugin.UnrealLink/Resources/Strings.Designer.cs
+++ src/dotnet/RiderPlugin.UnrealLink/Resources/Strings.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RiderPlugin.UnrealLink.Resources
+namespace RiderPlugin.UnrealLink.Resources
 {
   using System;
   using JetBrains.Application.I18n;
@@ -17,9 +17,10 @@
 
     static Strings()
     {
-      CultureContextComponent.Instance.WhenNotNull(Lifetime.Eternal, (lifetime, instance) =>
+      CultureContextComponent.Instance.Change.Advise(Lifetime.Eternal, args =>
       {
-        lifetime.Bracket(() =>
+          var instance = args.HasNew ? args.New : null;
+          if (instance != null)
           {
             ourResourceManager = new Lazy<JetResourceManager>(
               () =>
@@ -27,11 +28,11 @@
                 return instance
                   .CreateResourceManager("RiderPlugin.UnrealLink.Resources.Strings", typeof(Strings).Assembly);
               });
-          },
-          () =>
+          }
+          else
           {
             ourResourceManager = null;
-          });
+          };
       });
     }
     
