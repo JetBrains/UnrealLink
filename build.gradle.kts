d0376ea5a6f180d3dcbe909dbc536128907fed81
Ivan Migalev <ivan.migalev@jetbrains.com>
2025-06-12 19:10:52 +0200

    (RIDER-124253) UnrealLink: use dotnet.cmd to run Rider backend
    GitOrigin-RevId: 111259bf541ef2a9ba61f8486d49c9052de0b5ba

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import com.jetbrains.plugin.structure.base.utils.isFile
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.changelog.Changelog
+import org.jetbrains.intellij.platform.gradle.Constants
+import org.jetbrains.intellij.platform.gradle.tasks.BuildSearchableOptionsTask
 import org.jetbrains.intellij.platform.gradle.tasks.PrepareSandboxTask
 import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -9,8 +11,6 @@ import java.io.ByteArrayOutputStream
 import java.security.MessageDigest
 import kotlin.io.path.absolute
 import kotlin.io.path.isDirectory
-import org.jetbrains.intellij.platform.gradle.Constants
-import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import kotlin.io.path.pathString
 
 plugins {
@@ -238,6 +238,13 @@ tasks {
     instrumentCode {
         enabled = false
     }
+
+    fun JavaForkOptions.configureDotNet() {
+        systemProperty("rider.backend.dotnet.runtime.path", file("tools/dotnet.cmd").absolutePath)
+    }
+    withType<RunIdeTask>().configureEach { this.configureDotNet() }
+    withType<BuildSearchableOptionsTask>().configureEach { this.configureDotNet() }
+    withType<Test>().configureEach { this.configureDotNet() }
 
     withType<RunIdeTask>().configureEach {
         maxHeapSize = "4096m"
