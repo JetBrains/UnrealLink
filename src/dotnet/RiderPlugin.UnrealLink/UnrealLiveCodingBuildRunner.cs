ed3f9111d9bf961ce7d59fd4a2d54be4a6ddb161
Yuriy Mikhalev <yuriy.mikhalev@jetbrains.com>
2025-08-08 07:32:53 +0200

    [Unreal] Send hot reload messages to build output during the "Build solution" action
    GitOrigin-RevId: 87536c784a8c76575b7fa3a09d8b5a0c9f636276

--- src/dotnet/RiderPlugin.UnrealLink/UnrealLiveCodingBuildRunner.cs
+++ src/dotnet/RiderPlugin.UnrealLink/UnrealLiveCodingBuildRunner.cs
@@ -4,11 +4,15 @@ using JetBrains.Application.Threading.Tasks;
 using JetBrains.Collections.Viewable;
 using JetBrains.DataFlow;
 using JetBrains.Lifetimes;
+using JetBrains.Platform.BuildEvents;
+using JetBrains.Platform.MsBuildHost.Models;
 using JetBrains.ProjectModel;
 using JetBrains.ProjectModel.Features.SolutionBuilders;
 using JetBrains.ProjectModel.Tasks;
 using JetBrains.ReSharper.Feature.Services.Cpp.UE4;
+using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.ReSharperCpp.RiderPlugin.Build;
+using RiderPlugin.UnrealLink.Model;
 
 namespace RiderPlugin.UnrealLink;
 
@@ -44,16 +48,56 @@ public class UnrealLiveCodingBuildRunner(
         
         var def = Lifetime.Define(request.Lifetime);
         request.State.Value = BuildRunState.Running;
+        var parser = new CppUE4UbtBuildOutputParser();
+        var saver = new InFileBuildSessionSaver(def.Lifetime, request.EventsStoragePath,
+            InFileBuildSessionSaver.EnabledFeatures.Events | InFileBuildSessionSaver.EnabledFeatures.Projects);
 
         editorModel.IsHotReloadCompiling.Change.AdviseUntil(def.Lifetime, val =>
         {
             if (!val)
             {
-                request.State.Value = BuildRunState.Completed;
+                // Wait for last live coding messages
+                def.Lifetime.StartBackgroundAsync(async () =>
+                {
+                    await Task.Delay(100, def.Lifetime);
+                    await def.Lifetime.StartMainRead(() =>
+                    {
+                        request.State.Value = BuildRunState.Completed;
+                    });
+                });
                 return true;
             }
 
             return false;
+        });
+        editorModel.UnrealLog.Advise(def.Lifetime, logEvent =>
+        {
+            var category = logEvent.Info.Category.Data;
+            if (category == "LogLiveCoding")
+            {
+                
+                var outputKind = logEvent.Info.Type switch
+                {
+                    VerbosityType.Error => OutputKind.Error,
+                    VerbosityType.Warning => OutputKind.Warning,
+                    _ => OutputKind.Message
+                };
+                var message = logEvent.Text.Data;
+                switch (outputKind)
+                {
+                    case OutputKind.Error:
+                        request.AddError(message);
+                        break;
+                    case OutputKind.Warning:
+                        request.AddWarning(message);
+                        break;
+                }
+                
+                request.AddOutputBuildMessage(outputKind, message);
+                return;
+            }
+            
+            parser.ConsumeUnrealBuildToolMessage(request, null, new RdProjectId(-1), logEvent.Text.Data, saver);
         });
         editorModel.TriggerHotReload();
 
