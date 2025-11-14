ba0ac2b9a9342f175095f3e88847d368cc944d3a
AI Diff <aidiff@intellij>
2025-11-14 17:23:52 +0100

    [Autotests::Unreal] Replaced ProjectModelTestContext with UnrealProjectModelApiFacade
    GitOrigin-RevId: 857592b7e5452fb2804dae1a62b898736a702eea

--- src/rider/test/kotlin/com/jetbrains/rider/plugins/unreal/test/cases/integrationTests/projectModel/RefreshSolution.kt
+++ src/rider/test/kotlin/com/jetbrains/rider/plugins/unreal/test/cases/integrationTests/projectModel/RefreshSolution.kt
@@ -8,25 +8,21 @@ import com.jetbrains.rider.ideaInterop.vfs.VfsWriteOperationsHost
 import com.jetbrains.rider.model.RdFsRefreshRequest
 import com.jetbrains.rider.plugins.unreal.model.frontendBackend.rdRiderModel
 import com.jetbrains.rider.projectView.solution
-import com.jetbrains.rider.test.annotations.report.Feature
 import com.jetbrains.rider.test.annotations.Solution
 import com.jetbrains.rider.test.annotations.Subsystem
-import com.jetbrains.rider.test.annotations.TestSettings
 import com.jetbrains.rider.test.annotations.TestEnvironment
-import com.jetbrains.rider.test.contexts.ProjectModelTestContext
-import com.jetbrains.rider.test.contexts.UnrealTestContext
-import com.jetbrains.rider.test.enums.PlatformType
+import com.jetbrains.rider.test.annotations.TestSettings
+import com.jetbrains.rider.test.annotations.report.Feature
 import com.jetbrains.rider.test.enums.BuildTool
+import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.enums.sdk.SdkVersion
+import com.jetbrains.rider.test.facades.unreal.UnrealProjectModelApiFacade
 import com.jetbrains.rider.test.framework.combine
 import com.jetbrains.rider.test.framework.waitBackendAndWorkspaceModel
-import com.jetbrains.rider.test.scriptingApi.ProjectModelExp.dumpAfterAction
-import com.jetbrains.rider.test.scriptingApi.ProjectModelExp.withUnrealDump
 import com.jetbrains.rider.test.scriptingApi.copyAdditionalPluginToProject
 import com.jetbrains.rider.test.scriptingApi.waitPumping
 import com.jetbrains.rider.test.suplementary.RiderTestSolution
 import com.jetbrains.rider.test.unreal.UnrealTestLevelProject
-import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 import java.time.Duration
 
@@ -36,35 +32,29 @@ import java.time.Duration
 @TestEnvironment(platform = [PlatformType.WINDOWS_X64])
 class RefreshSolution : UnrealTestLevelProject() {
 
-  @BeforeMethod
-  fun updateDumpProfile() {
-    contexts.get<ProjectModelTestContext>().profile.dumpDirList.clear()
-    contexts.get<ProjectModelTestContext>().profile.dumpDirList.add(activeSolutionDirectory.resolve("Intermediate/ProjectFiles"))
-    contexts.get<ProjectModelTestContext>().profile.fileNames.add("$activeSolution.vcxproj.filters")
-  }
-
   @Solution(RiderTestSolution.Unreal.EmptyUProject)
   @Test(dataProvider = "AllEngines_slnOnly")
-  fun refreshSolution(@Suppress("UNUSED_PARAMETER") caseName: String,
-                      openWith: UnrealTestContext.UnrealProjectModelType, engine: UnrealEngine) {
-    val pmContext = contexts.get<ProjectModelTestContext>()
+  @Suppress("UNUSED_PARAMETER")
+  fun refreshSolution(caseName: String, openWith: UnrealProjectModelApiFacade.PMType, engine: UnrealEngine) {
+    withDump {
+      projectModelDumpProfile.dumpDirList.clear()
+      projectModelDumpProfile.dumpDirList.add(activeSolutionDirectory.resolve("Intermediate/ProjectFiles"))
+      projectModelDumpProfile.fileNames.add("$activeSolution.vcxproj.filters")
 
-    withUnrealDump(contexts) {
-      dumpAfterAction("Init", pmContext) {}
-      dumpAfterAction("Copy TestPlugin to project", pmContext) {
+      dumpAfterAction("Init") {}
+      dumpAfterAction("Copy TestPlugin to project") {
         copyAdditionalPluginToProject("EmptyTestPlugin")
-       }
+      }
 
-      dumpAfterAction("Invoking refresh solution", pmContext) {
+      dumpAfterAction("Invoking refresh solution") {
         project.solution.rdRiderModel.refreshProjects.fire()
         waitPumping(Duration.ofSeconds(1))
-        waitAndPump(Duration.ofSeconds(30),
-                    { !project.solution.rdRiderModel.refreshInProgress.value },
+        waitAndPump(Duration.ofSeconds(30), { !project.solution.rdRiderModel.refreshInProgress.value },
                     { "Response from UBT took longer than expected time" })
 
         VfsWriteOperationsHost.getInstance(project).refreshPaths(
-          RdFsRefreshRequest(listOf(activeSolutionDirectory.combine("Intermediate", "ProjectFiles", "$activeSolution.vcxproj").toRd()), false)
-        )
+          RdFsRefreshRequest(listOf(activeSolutionDirectory.combine("Intermediate", "ProjectFiles", "$activeSolution.vcxproj").toRd()),
+                             false))
         waitPumping(Duration.ofSeconds(1))
         waitBackendAndWorkspaceModel(project)
       }
