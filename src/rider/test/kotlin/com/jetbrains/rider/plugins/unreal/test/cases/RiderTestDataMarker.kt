918860160d84486a7719824425aa1526816bd741
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2025-12-29 09:12:52 +0200

    RIDER-134236 [rider-tests]: Replace java.io.File with java.nio.Path for Rider base test directories
    GitOrigin-RevId: 25ff08094ca16d1e38f42127fe92fdf8d59de45c

--- src/rider/test/kotlin/com/jetbrains/rider/plugins/unreal/test/cases/RiderTestDataMarker.kt
+++ src/rider/test/kotlin/com/jetbrains/rider/plugins/unreal/test/cases/RiderTestDataMarker.kt
@@ -1,13 +1,13 @@
 package com.jetbrains.rider.plugins.unreal.test.cases
 
 import com.jetbrains.rider.test.framework.testData.IRiderTestDataMarker
-import java.io.File
+import java.nio.file.Path
 
 @Suppress("unused")
 object RiderTestDataMarker : IRiderTestDataMarker {
-  override val testDataFromRoot: File
-    get() = File("src/rider/test/testData")
+  override val testDataFromRoot: Path
+    get() = Path.of("src/rider/test/testData")
 
-  override val pluginDirectoryInUltimate: File
-    get() = File("dotnet/Plugins/UnrealLink")
+  override val pluginDirectoryInUltimate: Path
+    get() = Path.of("dotnet/Plugins/UnrealLink")
 }
