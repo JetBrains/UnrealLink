2f8b978cf8fddcbc6e235f6a88e8a7235def9404
Aleksei Priakhin <aleksei.priakhin@jetbrains.com>
2026-05-14 15:20:27 +0200

    RIDER-138871 unreal-mcp: add C# UnrealMcpAssetQueryHandler binding RD asset index calls
    GitOrigin-RevId: 4e3d0ac6a5fbb68c2d8657e8a4864d1acb8744bc

---
+++ src/dotnet/RiderPlugin.UnrealLink/Mcp/UnrealMcpAssetQueryHandler.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using JetBrains.Application.Parts;
+using JetBrains.ProjectModel;
+using JetBrains.Rd.Tasks;
+using JetBrains.ReSharper.Feature.Services.Cpp.UE4.UEAsset;
+using JetBrains.ReSharper.Feature.Services.Cpp.UE4.UEAsset.Reader;
+using JetBrains.ReSharper.Feature.Services.Cpp.UE4.UEAsset.Reader.Entities.Properties;
+using JetBrains.ReSharper.Feature.Services.Cpp.UE4.UEAsset.Search;
+using RiderPlugin.UnrealLink.Model.FrontendBackend;
+
+namespace RiderPlugin.UnrealLink.Mcp;
+
+[SolutionComponent(InstantiationEx.LegacyDefault)]
+public class UnrealMcpAssetQueryHandler
+{
+    public UnrealMcpAssetQueryHandler([NotNull] UnrealHost unrealHost, [NotNull] UE4AssetsCache assetsCache)
+    {
+        unrealHost.PerformModelAction(model =>
+        {
+            model.SearchUnrealAssets.SetAsync((lt, request) =>
+                RdTask.Successful(SearchAssets(assetsCache, request)));
+
+            model.GetBlueprintHierarchy.SetAsync((lt, request) =>
+                RdTask.Successful(GetBlueprintHierarchy(assetsCache, request)));
+
+            model.SearchGameplayTags.SetAsync((lt, request) =>
+                RdTask.Successful(SearchGameplayTags(assetsCache, request)));
+
+            model.GetAssetProperties.SetAsync((lt, request) =>
+                RdTask.Successful(GetAssetProperties(assetsCache, request)));
+        });
+    }
+
+    [NotNull]
+    private static UnrealAssetSearchResponse SearchAssets([NotNull] UE4AssetsCache cache, [NotNull] UnrealAssetSearchRequest request)
+    {
+        var limit = request.Limit;
+        var results = new List<UnrealAssetInfo>();
+
+        if (request.BaseClass != null)
+        {
+            var baseClassNames = cache.GetBaseClassesByShortName(request.BaseClass);
+            var query = request.Query;
+            foreach (var cls in UE4SearchUtil.GetDerivedBlueprintClasses(baseClassNames, cache, uniqueNames: true))
+            {
+                if (results.Count >= limit) break;
+                if (!cls.ContainingFile.IsValid()) continue;
+                if (query != null && !cls.Name.Contains(query, StringComparison.OrdinalIgnoreCase))
+                    continue;
+                results.Add(new UnrealAssetInfo(cls.ContainingFile.GetLocation().FullPath, cls.Name, request.BaseClass));
+            }
+        }
+        else if (request.Query != null)
+        {
+            foreach (var file in cache.GetAssetFilesContainingWord(request.Query))
+            {
+                if (results.Count >= limit) break;
+                results.Add(new UnrealAssetInfo(file.GetLocation().FullPath, file.GetLocation().NameWithoutExtension, null));
+            }
+        }
+
+        return new UnrealAssetSearchResponse(results);
+    }
+
+    [NotNull]
+    private static UnrealBlueprintHierarchyResponse GetBlueprintHierarchy([NotNull] UE4AssetsCache cache, [NotNull] UnrealBlueprintHierarchyRequest request)
+    {
+        var baseClassNames = cache.GetBaseClassesByShortName(request.BaseClass);
+        var blueprints = UE4SearchUtil.GetDerivedBlueprintClasses(baseClassNames, cache, uniqueNames: true)
+            .Take(1000)
+            .Where(c => c.ContainingFile.IsValid())
+            .Select(c => new UnrealBlueprintInfo(c.Name, c.ContainingFile.GetLocation().FullPath))
+            .ToList();
+        return new UnrealBlueprintHierarchyResponse(blueprints);
+    }
+
+    [NotNull]
+    private static UnrealGameplayTagsResponse SearchGameplayTags([NotNull] UE4AssetsCache cache, [NotNull] UnrealGameplayTagsRequest request)
+    {
+        var tagLocations = request.Prefix != null
+            ? cache.FindGameplayTagsByPrefix(request.Prefix)
+            : cache.FindGameplayTagsByPrefix("");
+        var tags = tagLocations
+            .Where(t => t.File.IsValid())
+            .Take(request.Limit)
+            .Select(t => new UnrealGameplayTagInfo(t.TagName, t.File.GetLocation().FullPath))
+            .ToList();
+        return new UnrealGameplayTagsResponse(tags);
+    }
+
+    [NotNull]
+    private static UnrealAssetPropertiesResponse GetAssetProperties([NotNull] UE4AssetsCache cache, [NotNull] UnrealAssetPropertiesRequest request)
+    {
+        var fileName = System.IO.Path.GetFileNameWithoutExtension(request.AssetPath);
+        var sourceFile = cache.GetAssetFilesContainingWord(fileName)
+            .FirstOrDefault(f => f.IsValid() && f.GetLocation().FullPath == request.AssetPath);
+
+        if (sourceFile == null)
+            return new UnrealAssetPropertiesResponse(null, new List<UnrealAssetPropertyInfo>());
+
+        var accessor = cache.GetUEAssetFileAccessor(sourceFile);
+        string objectName = null;
+        var propertyInfos = new List<UnrealAssetPropertyInfo>();
+
+        if (accessor.TryGetValue(linker =>
+            linker.ExportMap.FirstOrDefault(e => e.IsClassDefaultObject),
+            out var cdoExport) && cdoExport != null)
+        {
+            objectName = cdoExport.ObjectStringName;
+            var properties = cdoExport.ReadProperties();
+            propertyInfos = properties
+                .Where(p => p.ValuePresentation != null)
+                .Select(p => new UnrealAssetPropertyInfo(p.Name, GetPropertyTypeName(p), p.ValuePresentation))
+                .ToList();
+        }
+
+        return new UnrealAssetPropertiesResponse(objectName, propertyInfos);
+    }
+
+    private static string GetPropertyTypeName([NotNull] IUEProperty prop)
+    {
+        var typeName = prop.GetType().Name;
+        if (typeName.Contains('`'))
+            typeName = typeName[..typeName.IndexOf('`')];
+        return typeName.StartsWith("UE", StringComparison.Ordinal) ? typeName[2..] : typeName;
+    }
+}
