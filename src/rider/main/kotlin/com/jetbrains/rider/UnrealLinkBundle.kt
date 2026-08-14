d14531841f8967611fab802db51d8ad15f374597
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-08-14 18:38:33 +0200

    [i18n] IJPL-203622 Replace DynamicBundle inheritance with composition using instance delegation
    GitOrigin-RevId: ee7f7086238d36eb343048828216eb94cf94f0c2

--- src/rider/main/kotlin/com/jetbrains/rider/UnrealLinkBundle.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/UnrealLinkBundle.kt
@@ -9,15 +9,16 @@ import java.util.function.Supplier
 @NonNls
 const val BUNDLE = "messages.UnrealLinkBundle"
 
-object UnrealLinkBundle : DynamicBundle(BUNDLE) {
+object UnrealLinkBundle {
+  private val instance = DynamicBundle(UnrealLinkBundle::class.java, BUNDLE)
     @JvmStatic
     @Nls
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-        return getMessage(key, *params)
+        return instance.getMessage(key, *params)
     }
 
     @JvmStatic
     fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): Supplier<String> {
-        return getLazyMessage(key, *params)
+        return instance.getLazyMessage(key, *params)
     }
 }
